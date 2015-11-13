12a0f39bc563c567e7b86af7c7be78f847f23b5a
UTycwang <utycwang@gmail.com>
2015-11-13 02:15:20 -0600

    reduce tests

--- Experiment-Test.py
+++ Experiment-Test.py
@@ -19,12 +19,12 @@ class MyTestCase(unittest.TestCase):
         r1 = Experiment.largest([3, -2, 3, -1, 4])
         self.assertEqual(r1, 4)
 
-    def test_t5(self):
-        r1 = Experiment.largest([-3, -2, -3, -1, -4])
-        self.assertEqual(r1, -1)
+    #def test_t5(self):
+    #   r1 = Experiment.largest([-3, -2, -3, -1, -4])
+    #   self.assertEqual(r1, -1)
 
-    def test_t7(self):
-        self.assertRaises(ValueError, Experiment.largest, [])
+    #def test_t7(self):
+    #    self.assertRaises(ValueError, Experiment.largest, [])
 
 #if __name__=='__main__':
 #    unittest.main()
