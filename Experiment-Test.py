5ae1b4603d6565bc5692c9ec5f6d4b315344cd94
UTycwang <utycwang@gmail.com>
2015-11-13 00:51:04 -0600

    nosetests cmd works

--- Experiment-Test.py
+++ Experiment-Test.py
@@ -26,5 +26,5 @@ class MyTestCase(unittest.TestCase):
     #def test_t7(self):
     #    self.assertRaises(ValueError, Experiment.largest, [])
 
-if __name__=='__main__':
-    unittest.main()
+#if __name__=='__main__':
+#    unittest.main()
